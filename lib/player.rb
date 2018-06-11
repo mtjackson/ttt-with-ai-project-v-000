5263338290c7bf06af8a9bb55ba75d056fbfc0a3
Mary Jackson <jackson.marytherese@gmail.com>
2018-06-11 23:09:48 +0000

    automatically backed up by learn

--- lib/player.rb
+++ lib/player.rb
@@ -8,5 +8,15 @@ class Player
     else
       @opps_token = "X"
     end
+    @win_combinations = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ]
   end
 end
