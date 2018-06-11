b799e0b33c7f4a1abaa2625f54e20fabd1d41ea7
Mary Jackson <jackson.marytherese@gmail.com>
2018-06-11 23:08:20 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -3,16 +3,7 @@ class Board
 
   def initialize
     @cells = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-    @win_combinations = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6]
-    ]
+  
   end
 
   def reset!
