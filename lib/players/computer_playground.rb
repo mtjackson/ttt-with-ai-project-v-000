b8b4ef48831b05fe16364d38fe4db6bf61d90ba1
Mary Jackson <jackson.marytherese@gmail.com>
2018-06-11 23:10:21 +0000

    automatically backed up by learn

--- lib/players/computer_playground.rb
+++ lib/players/computer_playground.rb
@@ -1,16 +1,16 @@
 
 #module Players
 #  class Computer < Player
-@win_combinations = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6]
-]
+    WIN_COMBINATIONS = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ]
     def move(board)
       #each time it plays, computer checks game.WIN_COMBINATIONS
       #first, check if computer has two values of a WIN_COMBINATIONS
